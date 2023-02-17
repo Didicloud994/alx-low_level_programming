7eb6b81598612b6848bc214418bf3b829805e3fd
“Didi” <aniodinaka@gmail.com>
2023-02-16 17:37:09 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int integer;
 long linteger;
 long long llinteger;
 float floatnumber;
-printf("Size of a char: %zu byte(s)\n",sizeof(ch));
-printf("Size of an int: %zu byte(s)\n",sizeof(integer));
-printf("Size of a long int: %zu byte(s)\n",sizeof(linteger));
-printf("Size of a long long int: %zu byte(s)\n",sizeof(llinteger));
-printf("Size of a float: %zu byte(s)\n",sizeof(floatnumber));
+printf("Size of a char: %zu byte(s)\n", sizeof(ch));
+printf("Size of an int: %zu byte(s)\n", sizeof(integer));
+printf("Size of a long int: %zu byte(s)\n", sizeof(linteger));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(llinteger));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatnumber));
 return (0);
 }
